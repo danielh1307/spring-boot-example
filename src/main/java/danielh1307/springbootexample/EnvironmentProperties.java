3e0da2fb9cfd10e7cb803da0ef24fe022f41313c
Daniel <hamm.daniel@gmail.com>
2018-11-04 16:24:06 +0100

    Added spring-boot-configuration-processor

--- src/main/java/danielh1307/springbootexample/EnvironmentProperties.java
+++ src/main/java/danielh1307/springbootexample/EnvironmentProperties.java
@@ -4,21 +4,30 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Component;
 
 @Component
+// @ConfigurationProperites supports relaxed binding and generation of metadata
 @ConfigurationProperties("environment")
 // @Validated can be used to validate properties with JSR 303 (Bean Validation)
 // you can put annotations to properties like @Min(10)
 // but BeanValidation must be accessible on classpath, e.g. with spring-boot-starter-validation
 public class EnvironmentProperties {
 
+    // we need to have a comment in the following format to get a description in spring-configuration-metadata.json
+    /**
+     * this is a very important property, since it is property 1
+     */
     private String prop1;
 
+    /**
+     * a little less important than property 1, but still important!
+     */
     private String prop2;
 
-    String getProp1() {
+    // if the getter is not public, there will be no entry generated for META-INF/spring-configuration-metadata.json
+    public String getProp1() {
         return prop1;
     }
 
-    String getProp2() {
+    public String getProp2() {
         return prop2;
     }
 
