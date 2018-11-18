b4f15dcfecfde4d9062d4c30468b6bc0ae3d3f1a
Daniel <hamm.daniel@gmail.com>
2018-11-18 18:21:59 +0100

    Removed unused imports

--- src/test/java/danielh1307/springbootexample/films/FilmControllerTest.java
+++ src/test/java/danielh1307/springbootexample/films/FilmControllerTest.java
@@ -6,11 +6,8 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
